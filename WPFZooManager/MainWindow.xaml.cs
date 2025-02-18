dc3b956aa9a0d7bccfe2e395c38b8aa7f1caf39a
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-02-18 16:37:54 +0430

    ConnectionString By this you can access data from a database(SQL Server).

--- WPFZooManager/MainWindow.xaml.cs
+++ WPFZooManager/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 
 namespace WPFZooManager
 {
@@ -23,6 +24,7 @@ namespace WPFZooManager
         public MainWindow()
         {
             InitializeComponent();
+            string connectionString = ConfigurationManager.ConnectionStrings["WPFZooManager.Properties.Settings.ManagementZoodbConnectionString"].ConnectionString;
         }
     }
 }
--- WPFZooManager/Properties/Settings.Designer.cs
+++ WPFZooManager/Properties/Settings.Designer.cs
@@ -8,22 +8,30 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPFZooManager.Properties
-{
-
-
+namespace WPFZooManager.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=JAHED\\JAHEDSQL;Initial Catalog=ManagementZoodb;Persist Security Info=" +
+            "True;User ID=sa;Password=jahed;Pooling=False;Encrypt=True;TrustServerCertificate" +
+            "=True")]
+        public string ManagementZoodbConnectionString {
+            get {
+                return ((string)(this["ManagementZoodbConnectionString"]));
             }
         }
     }
